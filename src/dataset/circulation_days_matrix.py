900531b2f3732888422ccc6343d045a0d5073d39
Damiano Ravalico <damianoravalico.uni@gmail.com>
2023-07-11 16:32:54 +0200

    Removed useless code, again

--- src/dataset/circulation_days_matrix.py
+++ src/dataset/circulation_days_matrix.py
@@ -8,6 +8,5 @@ def generate_circulation_days_matrix(L, O):
         suppliers_circulation_list = []
         for _ in L:
             suppliers_circulation_list.append(np.random.randint(math.ceil(max(O) / o), max(O) + 1, dtype=int))
-        print(suppliers_circulation_list)
         circulation_matrix.append(suppliers_circulation_list)
     return circulation_matrix
