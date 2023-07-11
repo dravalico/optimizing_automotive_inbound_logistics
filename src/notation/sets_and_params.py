0d35c46d6d3e552b9e2b85946bc7854be292cbf3
Damiano Ravalico <damianoravalico.uni@gmail.com>
2023-07-11 16:19:21 +0200

    Renaming

--- src/notation/sets_and_params.py
+++ src/notation/sets_and_params.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from src.dataset.dataset import n_suppliers, LTL_zones, distance_of_suppliers, daily_demand_of_SKUs_of_suppliers, \
     load_carrier_invest_costs, load_carrier_rental_costs
 from src.dataset.freight_cost_matrix import generate_freight_cost_matrix_LTL, generate_freight_cost_matrix_CES
@@ -15,31 +15,31 @@ K = range(10)  # Set of all weight classes in the freight cost matrix of courier
 
 # Parameters of suppliers
 d_i = daily_demand_of_SKUs_of_suppliers  # Demand of supplier i in L per day [#/day]
-r_iz = numpy.random.randint(2, size=(len(L), len(Z)))  # Allocation of supplier L to zone Z (1 if true, 0 if false)
+r_iz = np.random.randint(2, size=(len(L), len(Z)))  # Allocation of supplier L to zone Z (1 if true, 0 if false)
 
 # Parameters for transportation process
-f_i_wq = numpy.random.randn(1, len(L))  # Parameter reflecting the relationship between w_ij and q_ij [kg/m^3]
+f_i_wq = np.random.randn(1, len(L))  # Parameter reflecting the relationship between w_ij and q_ij [kg/m^3]
 omega_LTL = 30  # Minimal weight required by regional forwarder service provider for the delivered parts [kg]
 Q_min = 30  # Minimum order quantity for CES and FTL [kg]
 
 # Parameters for goods-entry
 Cap_GI = 85  # Capacity regarding goods [# trucks/day]
 Cap_WK = 40  # Capacity regarding goods-entry for courier and express service [# trucks/day]
-Cap_h = numpy.random.randint(10000, 20000, len(H))  # Capacity in storage area h in H [storage places]
+Cap_h = np.random.randint(10000, 20000, len(H))  # Capacity in storage area h in H [storage places]
 Cap_L = 80  # Volume capacity of a FTL truck [m^3]
 Cap_WL = 22000  # Weight capacity of a FTL truck [kg]
 Cap_K = 3500  # Weight capacity of a CES truck [kg]
 
 # Parameters for the warehouse
-SS_i = 200 * numpy.ones(len(L), dtype=int)  # Safety stock for supplier i in L [storage places]
-f_hi_qp = (1 / 1.92) * numpy.ones((len(H), len(L)), dtype=int)  # Coefficient from volume to storage places
+SS_i = 200 * np.ones(len(L), dtype=int)  # Safety stock for supplier i in L [storage places]
+f_hi_qp = (1 / 1.92) * np.ones((len(H), len(L)), dtype=int)  # Coefficient from volume to storage places
 
 # Parameters for planning and cost calculation
 # TODO Implement piece-wise linear cost-per-km function
 C_i_D = [(500 + 1.0 * val) for val in distance_of_suppliers]  # Fix cost for transportation per truck for i in L [€]
 B_ib_p = generate_freight_cost_matrix_LTL(Q, Z, L)  # Prices of the weight class b in Q for LTL for i in L [€/kg]
 B_k_pCES = generate_freight_cost_matrix_CES(K)  # Prices of the weight class k in K for CES [€/kg]
-f_i_SLC = numpy.random.randint(2, len(L))  # Parameter indicating if supplier i in L needs any SLC for the shipment
+f_i_SLC = np.random.randint(2, len(L))  # Parameter indicating if supplier i in L needs any SLC for the shipment
 C_i_dR = load_carrier_rental_costs  # Rental cost for load carriers for supplier i in L to satisfy one day's demand [€/day]
 C_i_dI = load_carrier_invest_costs  # Investment cost for load carriers supplier i in L to satisfy one day's demand [€/day]
 # u_io_R =  # Circulation days for universal load carriers i in L and o in O [days]
