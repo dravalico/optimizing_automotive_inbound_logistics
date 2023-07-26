d8bc98fb1f5033f1368da5bfe96abe2027caf6e5
Damiano Ravalico <damianoravalico.uni@gmail.com>
2023-07-26 10:07:04 +0200

    Fixed a param

--- src/dataset/params.py
+++ src/dataset/params.py
@@ -21,7 +21,7 @@ K = range(
 # Dataset
 number_of_SKU_ordered = [max(1, int(i)) for i in generate_samples(1, 6.89, 110, sets.part_numbers, 1, 99)]
 daily_demand_of_SKUs_of_suppliers = [int(i) + 1 for i in
-                                     generate_samples(1.41, 3439.98, 280573, sets.n_suppliers, 0.5, 99.5)]
+                                     generate_samples(1.41, 3439.98, 280573.60, sets.n_suppliers, 0.5, 99.5)]
 daily_demand_volume = generate_samples(0.00, 2.53, 69.70, sets.n_suppliers, 1, 99)
 daily_demand_weight = generate_samples(0.00, 357.74, 9994.02, sets.n_suppliers, 1, 99)
 load_carrier_rental_costs = generate_samples(0.00, 0.08, 1.27, sets.n_suppliers, 1, 99)
