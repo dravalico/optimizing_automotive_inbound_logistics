08d70eaa7fee3674d4f3344428628310283e56a0
Damiano Ravalico <damianoravalico.uni@gmail.com>
2023-07-27 11:30:40 +0200

    Added plot function

--- src/dataset/generation/freight_cost_matrix.py
+++ src/dataset/generation/freight_cost_matrix.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def _compressed_logistic_function(x, scale, shift, min_value, max_value):
@@ -28,3 +29,19 @@ def generate_freight_cost_matrix_CES(K):
     for i in K:
         prices[i] = round(_compressed_logistic_function(i, -0.6, 4.5, 1, 10), 2)
     return prices
+
+
+def plot_sigmoid_function():
+    x = np.linspace(0, 10, 100)
+    y = _compressed_logistic_function(x, -0.6, 4.5, 1, 10)
+    plt.plot(x, y)
+    plt.xlabel('Weight class')
+    plt.ylabel('Price')
+    integer_x_values = np.arange(10)
+    integer_y_values = _compressed_logistic_function(integer_x_values, -0.6, 4.5, 1, 10)
+    plt.scatter(integer_x_values, integer_y_values, color='red', label='Price-per-class')
+    plt.legend()
+    plt.show()
+
+
+plot_sigmoid_function()
