f9d314b702d3306d9c9b00b6b913eb3eea4be454
Damiano Ravalico <damianoravalico.uni@gmail.com>
2023-07-27 11:32:15 +0200

    Plot of lognormal distribution of dataset

--- src/anal/dataset_avg.py
+++ src/anal/dataset_avg.py
@@ -1,5 +1,6 @@
 import numpy as np
-from src.dataset.generation.lognormal_distribution import generate_lognormal_distribution_samples
+from src.dataset.generation.lognormal_distribution import generate_samples, generate_lognormal_distribution_samples
+import matplotlib.pyplot as plt
 
 
 def average_summary_statistics(min_val, avg_val, max_val, size, name):
@@ -22,9 +23,20 @@ def average_summary_statistics(min_val, avg_val, max_val, size, name):
     print(f"{name} - Average Max: {average_max}")
 
 
+def plot_lognormal_distribution(min_val, avg_val, max_val, size):
+    lognormal_samples = generate_samples(min_val, avg_val, max_val, size)
+    plt.hist(lognormal_samples, bins='auto', density=False, color='blue', label='Samples')
+    plt.xlim(0, 22000)
+    plt.xlabel('Demand [# part numbers/day]')
+    plt.ylabel('Frequency')
+    plt.legend()
+    plt.show()
+
+
 average_summary_statistics(1.41, 3439.98, 280573.60, 570, "Demand [# part numbers/day]")
 average_summary_statistics(0.00, 2.53, 69.70, 570, "Demand [m^3/day]")
 average_summary_statistics(0.00, 357.74, 9994.02, 570, "Demand [kg/day]")
 average_summary_statistics(0.00, 0.08, 1.27, 570, "Load carrier rental costs")
 average_summary_statistics(0.00, 1.43, 133.33, 570, "Load carrier invest costs")
 average_summary_statistics(6.00, 538.38, 2547.00, 570, "Distance of suppliers")
+plot_lognormal_distribution(1.41, 3439.98, 280573.60, 570)
