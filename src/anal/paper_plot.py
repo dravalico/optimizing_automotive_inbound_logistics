3d9cd36593b146daed7ff534a936568189c232f6
Damiano Ravalico <damianoravalico.uni@gmail.com>
2023-07-27 12:42:54 +0200

    Merged two scripts

--- src/anal/paper_plot.py
+++ src/anal/paper_plot.py
@@ -58,6 +58,28 @@ def plot_pie_chart_of_transport_share():
     plt.show()
 
 
+def cost_suddivision():
+    grouped_data = df.groupby('n_suppliers').agg({
+        'trans_costs': 'mean',
+        'order_cost': 'mean',
+        'load_car_rent': 'mean',
+        'load_car_invest': 'mean'
+    }).reset_index()
+
+    categories = ['trans_costs', 'order_cost', 'load_car_rent', 'load_car_invest']
+    positions = range(len(grouped_data))
+    bar_width = 0.2
+    for i, category in enumerate(categories):
+        plt.bar([p + i * bar_width for p in positions], grouped_data[category], bar_width, label=category)
+    plt.xticks([p + 1.5 * bar_width for p in positions], grouped_data['n_suppliers'])
+    plt.xlabel('Suppliers')
+    plt.ylabel('Mean Values')
+    plt.title('Mean Values of Categories per Supplier')
+    plt.legend(title='Categories')
+    plt.tight_layout()
+    plt.show()
+
+
 def plot_pie_chart_of_trasport_share_of_paper():
     labels = ["FTL", "CES", "LTL"]
     colors = ['lightgreen', 'lightcoral', 'lightskyblue']
@@ -72,3 +94,4 @@ plot_gap_for_suppliers()
 plot_execution_time_for_suppliers()
 plot_pie_chart_of_transport_share()
 plot_pie_chart_of_trasport_share_of_paper()
+cost_suddivision()
